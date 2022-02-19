2d9cd8f6b3800c14783ed9f994fba381a658b92d
adithyang4media <86531328+adithyang4media@users.noreply.github.com>
2022-02-19 23:45:43 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import youtube_dl
 import os
 from pytube import YouTube
 client = commands.Bot(command_prefix='!')
+await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(client.guilds)} servers!"))
 @client.command()
 async def play(ctx, url_: str):
     song_there = os.path.isfile("song.mp3")
